f69695b22898d436a89a87420f64c5ab5e885f0b
Amanda Hinton <hello@amandahinton.com>
2021-09-21 14:21:39 -0700

    Create database tables/models

---
+++ app/models/bookmark.py
@@ -0,0 +1,17 @@
+from .db import db
+
+class Bookmark(db.Model):
+    __tablename__ = 'bookmarks'
+
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey("users.id"), nullable=False, unique=True)
+    article_id = db.Column(db.Integer, db.ForeignKey("articles.id"), nullable=False, unique=True)
+    user_details = db.relationship("User", back_populates="bookmark_details")
+    article_details = db.relationship("Article", back_populates="bookmark_details")
+
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'user_id': self.username,
+            'article_id': self.article_id,
+        }
--- app/seeds/users.py
+++ app/seeds/users.py
@@ -1,27 +1,26 @@
 from app.models import db, User
 
-
-# Adds a demo user, you can add other users here if you want
 def seed_users():
-    demo = User(
-        username='Demo', email='demo@aa.io', password='password')
-    marnie = User(
-        username='marnie', email='marnie@aa.io', password='password')
-    bobbie = User(
-        username='bobbie', email='bobbie@aa.io', password='password')
+    admin = User(
+        username='Chromaculture_Team', email='chromaculture@gmail.com', password='password')
+    guest = User(
+        username='GambogeGuest', email='demo@aa.io', password='password', favorite_color='neon yellow')
+    magenta = User(
+        username='MagentaMax', email='magenta@aa.io', password='password', favorite_color='bubblegum pink')
+    cerulean = User(
+        username='CeruleanSarah', email='cerulean@aa.io', password='password', favorite_color='the blues of the Caribbean Sea')
 
-    db.session.add(demo)
-    db.session.add(marnie)
-    db.session.add(bobbie)
+    db.session.add(admin)
+    db.session.add(guest)
+    db.session.add(magenta)
+    db.session.add(cerulean)
 
     db.session.commit()
 
 
-# Uses a raw SQL query to TRUNCATE the users table.
-# SQLAlchemy doesn't have a built in function to do this
-# TRUNCATE Removes all the data from the table, and RESET IDENTITY
-# resets the auto incrementing primary key, CASCADE deletes any
-# dependent entities
+# TRUNCATE removes all data from table
+# RESET IDENTITY resets auto-incrementing primary key
+# CASCADE deletes any dependent entities
 def undo_users():
     db.session.execute('TRUNCATE users RESTART IDENTITY CASCADE;')
     db.session.commit()
