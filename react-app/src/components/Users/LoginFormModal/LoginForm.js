d987d06a9ba5c2e70a595f15434250bf56ece44e
Amanda Hinton <hello@amandahinton.com>
2021-09-26 23:51:35 -0700

    Fix placement of ribbon-shaped register and login

--- react-app/src/components/Users/LoginFormModal/LoginForm.js
+++ react-app/src/components/Users/LoginFormModal/LoginForm.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { Redirect, useHistory } from 'react-router-dom';
 import { login } from '../../../store/session';
+import "../users.css"
 
 const LoginForm = () => {
     const [errors, setErrors] = useState([]);
