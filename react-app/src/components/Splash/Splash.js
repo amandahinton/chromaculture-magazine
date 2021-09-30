8f6f84db3cde7753349b6d745d88ed8fd3a98ac8
Amanda Hinton <hello@amandahinton.com>
2021-09-29 19:25:19 -0700

    Style homepage with media query breakpoints

--- react-app/src/components/Splash/Splash.js
+++ react-app/src/components/Splash/Splash.js
@@ -16,20 +16,10 @@ function Splash( {articles} ) {
           document.querySelector("body").style.overflow = 'visible';
         }
       }, [articles, showArticleModal]);
-    console.log("ARTICLES ----->>>", articles)
 
 
     return (
         <div className="splash-container">
-
-            <div className="splash-text">
-                <h1 className="splash-title">Welcome to Chromaculture!</h1>
-                <h2 className="splash-subtitle">The Digital Magazine for Color Lovers</h2>
-                <h3 className="splash-description">Our inspiring collection of stories from around the web about palettes, pigments, and color-related projects will take you to surprising places in the spectrum! Chromaculture blends color theory, science, art history, industry information, and pop culture in a way that is modern, informative, provocative, and playful. </h3>
-            </div>
-
-
-
 
             <NavLink exact to="/articles/16" className="splash-main-feature-div">
             <div className="main-feature-detail-text">
@@ -42,10 +32,8 @@ function Splash( {articles} ) {
 
             </NavLink>
 
-
-
             <div className="splash-highlight-div">
-                <NavLink exact to="/articles/15" className="splash-article-highlight-one">
+                <NavLink exact to="/articles/15" className="splash-highlight-one">
                     <h3 className="splash-highlight-title">{articles[14].title}</h3>
                     <p className="splash-highlight-byline">{articles[14].author}  |  {articles[14].source}</p>
                     <p className="splash-highlight-description">{articles[14].description}</p>
@@ -55,7 +43,7 @@ function Splash( {articles} ) {
 
                 <img className="splash-highlight-image" src="https://images.unsplash.com/photo-1600832331197-ad575931911b" alt="fanned pantone cards" />
 
-                <NavLink exact to="/articles/1" className="splash-article-highlight-two">
+                <NavLink exact to="/articles/1" className="splash-highlight-two">
                     <h3 className="splash-highlight-title">{articles[0].title}</h3>
                     <p className="splash-highlight-byline">{articles[0].author}  |  {articles[0].source}</p>
                     <p className="splash-highlight-description">{articles[0].description}</p>
@@ -63,41 +51,46 @@ function Splash( {articles} ) {
                 </NavLink>
             </div>
 
-
-
-
-
-
-            <ul className="splash-cards-div">
-                <ArticleCard className="splash-card" article={articles[6]} showArticleModal={showArticleModal} setShowArticleModal={setShowArticleModal} />
-            </ul>
-
-            <ul className="splash-cards-div">
-                <ArticleCard className="splash-card" article={articles[9]} showArticleModal={showArticleModal} setShowArticleModal={setShowArticleModal} />
-            </ul>
-
-            <ul className="splash-cards-div">
-                <ArticleCard className="splash-card" article={articles[11]} showArticleModal={showArticleModal} setShowArticleModal={setShowArticleModal} />
-            </ul>
-
-            <ul className="splash-cards-div">
-                <ArticleCard className="splash-card" article={articles[12]} showArticleModal={showArticleModal} setShowArticleModal={setShowArticleModal} />
-            </ul>
-
-
-            <div className="splash-image-grid">
-
-                <img className="splash-image" src="https://images.unsplash.com/photo-1520207756688-175b32d0917d" alt="pink lightbulb" />
-
-                <img className="splash-image" src="https://images.unsplash.com/photo-1596798647070-c015ec23610d" alt="flamingo feathers" />
-                <img className="splash-image" src="https://images.unsplash.com/photo-1516478784322-0b77268e6c64" alt="candy sprinkles" />
-                <img className="splash-image" src="https://images.unsplash.com/photo-1460661419201-fd4cecdf8a8b" alt="dirty paintbrushes" />
-                <img className="splash-image" src="https://images.unsplash.com/photo-1534287610067-865f1f2ad7e5" alt="blue door" />
-                <img className="splash-image" src="https://images.unsplash.com/photo-1565793388498-6eb52f629afe" alt="spraypainted rotary phone" />
-                <img className="splash-image" src="https://images.unsplash.com/photo-1623929147453-b0abf89a9363" alt="coral reef" />
-                <img className="splash-image" src="https://images.unsplash.com/photo-1509652839609-d94a8ad572db" alt="spilled ink" />
+            <div className="splash-description-div">
+                <h1 className="splash-title">Welcome to Chromaculture!</h1>
+                <h2 className="splash-subtitle">The Digital Magazine for Color Lovers</h2>
+                <p className="splash-description">Our inspiring collection of stories from around the web about palettes, pigments, and color-related projects will take you to surprising places in the spectrum! Chromaculture blends color theory, science, art history, industry information, and pop culture in a way that is modern, informative, provocative, and playful.</p>
             </div>
 
+            <div className="splash-cards-container">
+
+                <div className="splash-cards-duo">
+                <ul className="splash-cards-div">
+                    <ArticleCard className="splash-card" article={articles[6]} showArticleModal={showArticleModal} setShowArticleModal={setShowArticleModal} />
+                </ul>
+
+                <img className="splash-midcard-image" src="https://images.unsplash.com/photo-1520207756688-175b32d0917d" alt="pink lightbulb" />
+                </div>
+
+                <div className="splash-cards-duo">
+                <ul className="splash-cards-div">
+                    <ArticleCard className="splash-card" article={articles[9]} showArticleModal={showArticleModal} setShowArticleModal={setShowArticleModal} />
+                </ul>
+
+                <img className="splash-midcard-image" src="https://images.unsplash.com/photo-1509652839609-d94a8ad572db" alt="spilled ink" />
+                </div>
+
+                <div className="splash-cards-duo">
+                <img className="splash-midcard-image" src="https://images.unsplash.com/photo-1596798647070-c015ec23610d" alt="flamingo feathers" />
+
+                <ul className="splash-cards-div">
+                    <ArticleCard className="splash-card" article={articles[11]} showArticleModal={showArticleModal} setShowArticleModal={setShowArticleModal} />
+                </ul>
+                </div>
+
+                <div className="splash-cards-duo">
+                <img className="splash-midcard-image" src="https://images.unsplash.com/photo-1565793388498-6eb52f629afe" alt="spraypainted rotary phone" />
+
+                <ul className="splash-cards-div">
+                    <ArticleCard className="splash-card" article={articles[12]} showArticleModal={showArticleModal} setShowArticleModal={setShowArticleModal} />
+                </ul>
+                </div>
+            </div>
         </div>
     );
 
